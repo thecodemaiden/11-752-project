61d670d9070c8e45ac8a0d204ed504b8d211fab0
Adeola Bannis <thecodemaiden@gmail.com>
2014-02-27 15:12:28 -0500

    Taking submission and going to the next clip

--- app/views.py
+++ app/views.py
@@ -6,7 +6,16 @@ from app import app
 def index():
 	return render_template('index.html')
 
+@app.route('/submit/', methods=['POST'])
+def save_input():
+	last_n = request.form['n']
+	data = request.form['trans']
+	print data, last_n
+	n = int(last_n)+1
+	return present_file(str(n))
+
+@app.route('/transcribe/', defaults={'num':1})
 @app.route('/transcribe/<num>')
 def present_file(num):
 	filename = app.config['AUDIO_FILES'][int(num)]
-	return render_template('index.html', filename=filename)
+	return render_template('index.html', filename=filename, n=num)
