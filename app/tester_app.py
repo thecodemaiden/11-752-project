386df4d68adbfd2640ab7d9435f7da2d023beb1d
Michael Nye <thenyeguy@gmail.com>
2014-02-28 12:39:15 -0500

    Added training sequence

--- app/tester_app.py
+++ app/tester_app.py
@@ -8,15 +8,22 @@ from flask import *
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+TRAIN_FOLDER = os.path.join(APP_ROOT, 'static/training')
 AUDIO_FOLDER = os.path.join(APP_ROOT, 'static/audio')
 RESULTS_FOLDER = os.path.join(APP_ROOT, 'results')
 
+TRAIN_FILES = [f for f in os.listdir(TRAIN_FOLDER) if 
+        os.path.isfile(os.path.join(TRAIN_FOLDER,f)) and
+        f.endswith('.wav')]
 AUDIO_FILES = [f for f in os.listdir(AUDIO_FOLDER) if 
-        os.path.isfile(os.path.join(AUDIO_FOLDER,f))]
+        os.path.isfile(os.path.join(AUDIO_FOLDER,f)) and
+        f.endswith('.wav')]
 NEXT_ID = len(os.listdir(RESULTS_FOLDER))+1
 
 app.config['APP_ROOT'] = APP_ROOT
+app.config['TRAIN_FOLDER'] = TRAIN_FOLDER
 app.config['AUDIO_FOLDER'] = AUDIO_FOLDER
+app.config['TRAIN_FILES'] = TRAIN_FILES
 app.config['AUDIO_FILES'] = AUDIO_FILES
 app.config['NEXT_ID'] = NEXT_ID
 
@@ -57,6 +64,62 @@ def splash(userid):
     return render_template('index.html',userid=userid, hard=hard)
 
 
+@app.route('/train/<userid>/', defaults={'num':'0'})
+@app.route('/train/<userid>/<num>')
+def present_train(userid, num):
+    # If we have completed all training examples, redirect
+    num = int(num)
+    if num >= len(app.config['TRAIN_FILES']):
+        return redirect('/begin/'+userid)
+
+    # Load the file for this test
+    filename = app.config['TRAIN_FILES'][num]
+    return render_template('train.html', filename=filename, n=num, userid=userid)
+
+
+@app.route('/submittrain/<userid>', methods=['POST'])
+def score_train(userid):
+    # Get the data
+    last_n = request.form['n']
+    data = request.form['trans']
+
+    # Write their transcription to the results file
+    resultLine = 'Training utterance ' + last_n + ' at timestamp ' + \
+            str(time.time()) + '\n\t' + data + '\n'
+    resultFile = open(app.config['APP_ROOT'] + '/results/' + str(userid) + \
+            '.txt', 'a')
+    resultFile.write(resultLine)
+
+    # Get the actual transcription
+    audiofilename = app.config['TRAIN_FILES'][int(last_n)]
+    transcriptionfilename = audiofilename.replace('.wav','.txt')
+    transcriptionfile = open(app.config['TRAIN_FOLDER'] + '/' + \
+            transcriptionfilename)
+    transcriptionlines = transcriptionfile.readlines()
+
+    transcription = " "
+    if (int(userid) % 2 == 0):
+        transcription = transcriptionlines[0]
+    else:
+        transcription = transcriptionlines[1]
+
+    return render_template('train.html', filename=audiofilename, \
+            transcription=transcription, user=data, n=int(last_n), userid=userid)
+
+@app.route('/begin/<userid>')
+def begin(userid):
+    return render_template('begin.html', userid=userid)
+
+
+@app.route('/transcribe/<userid>/', defaults={'num':'0'})
+@app.route('/transcribe/<userid>/<num>')
+def present_file(userid, num):
+    # Load the file for this test
+    num = int(num)
+    filename = app.config['AUDIO_FILES'][num]
+    return render_template('transcribe.html', filename=filename, n=num, userid=userid)
+
+
 @app.route('/submit/<userid>', methods=['POST'])
 def save_input(userid):
     # Get the data
@@ -72,18 +135,10 @@ def save_input(userid):
 
     # Increment utterance number and redirect
     n = int(last_n)+1
-    if n > len(app.config['AUDIO_FILES']):
+    if n >= len(app.config['AUDIO_FILES']):
         return redirect('/thanks/')
     else:
         return redirect('/transcribe/'+str(userid)+'/'+str(n))
-
-
-@app.route('/transcribe/<userid>/', defaults={'num':1})
-@app.route('/transcribe/<userid>/<num>')
-def present_file(userid, num):
-    # Load the file for this test
-    filename = app.config['AUDIO_FILES'][int(num)]
-    return render_template('transcribe.html', filename=filename, n=num, userid=userid)
 
 
 @app.route('/thanks/')
